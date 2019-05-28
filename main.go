df7fb4a9581e52175be774438aa0d9b10fd882a2
Emanuel Zabka <emanuelzabka@gmail.com>
2019-05-28 08:06:44 -0300

    Auto generating keys, beginning block data encrypt

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/hex"
 	"flag"
 	"fmt"
+	"math/rand"
 	"os"
+	"time"
 )
 
 const (
@@ -26,10 +29,10 @@ var finalPermutation []int = []int{
 }
 
 var keyPermutation []int = []int{
-	57, 49, 41, 33, 25, 17,  9,  1, 58, 50, 42, 34, 26, 18,
-	10,  2, 59, 51, 43, 35, 27, 19, 11,  3, 60, 52, 44, 36,
-	63, 55, 47, 39, 31, 23, 15,  7, 62, 54, 46, 38, 30, 22,
-	14,  6, 61, 53, 45, 37, 29, 21, 13,  5, 28, 20, 12,  4,
+	57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18,
+	10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36,
+	63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22,
+	14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4,
 }
 
 var keyShiftsPerRound []int = []int{
@@ -50,65 +53,65 @@ var expansionPermutation []int = []int{
 	24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1,
 }
 
-var sboxes [][]int = [][]int {
+var sboxes [][]int = [][]int{
 	{
-		14,  4, 13, 1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9, 0,  7,
-		 0, 15,  7, 4, 14,  2, 13,  1, 10,  6, 12, 11,  9,  5, 3,  8,
-		 4,  1, 14, 8, 13,  6,  2, 11, 15, 12,  9,  7,  3, 10, 5,  0,
-		15, 12,  8, 2,  4,  9,  1,  7,  5, 11,  3, 14, 10,  0, 6, 13,
+		14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7,
+		0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8,
+		4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0,
+		15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13,
 	},
 	{
-		15,  1,  8, 14,  6, 11,  3,  4,  9, 7,  2, 13, 12, 0,  5, 10,
-		 3, 13,  4,  7, 15,  2,  8, 14, 12, 0,  1, 10,  6, 9, 11,  5,
-		 0, 14,  7, 11, 10,  4, 13,  1,  5, 8, 12,  6,  9, 3,  2, 15,
-		13,  8, 10,  1,  3, 15,  4,  2, 11, 6,  7, 12,  0, 5, 14,  9,
+		15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10,
+		3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5,
+		0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15,
+		13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9,
 	},
 	{
-		10,  0,  9, 14, 6,  3, 15,  5,  1, 13, 12,  7, 11,  4,  2,  8,
-		13,  7,  0,  9, 3,  4,  6, 10,  2,  8,  5, 14, 12, 11, 15,  1,
-		13,  6,  4,  9, 8, 15,  3,  0, 11,  1,  2, 12,  5, 10, 14,  7,
-		 1, 10, 13,  0, 6,  9,  8,  7,  4, 15, 14,  3, 11,  5,  2, 12,
+		10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8,
+		13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1,
+		13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7,
+		1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12,
 	},
 	{
-		 7, 13, 14, 3,  0,  6,  9, 10,  1, 2, 8,  5, 11, 12,  4, 15,
-		13,  8, 11, 5,  6, 15,  0,  3,  4, 7, 2, 12,  1, 10, 14,  9,
-		10,  6,  9, 0, 12, 11,  7, 13, 15, 1, 3, 14,  5,  2,  8,  4,
-		 3, 15,  0, 6, 10,  1, 13,  8,  9, 4, 5, 11, 12,  7,  2, 14,
+		7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15,
+		13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9,
+		10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4,
+		3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14,
 	},
 	{
-		 2, 12,  4,  1,  7, 10, 11,  6,  8,  5,  3, 15, 13, 0, 14,  9,
-		14, 11,  2, 12,  4,  7, 13,  1,  5,  0, 15, 10,  3, 9,  8,  6,
-		 4,  2,  1, 11, 10, 13,  7,  8, 15,  9, 12,  5,  6, 3,  0, 14,
-		11,  8, 12,  7,  1, 14,  2, 13,  6, 15,  0,  9, 10, 4,  5,  3,
+		2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9,
+		14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6,
+		4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14,
+		11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3,
 	},
 	{
-		12,  1, 10, 15, 9,  2,  6,  8,  0, 13,  3,  4, 14,  7,  5, 11,
-		10, 15,  4,  2, 7, 12,  9,  5,  6,  1, 13, 14,  0, 11,  3,  8,
-		 9, 14, 15,  5, 2,  8, 12,  3,  7,  0,  4, 10,  1, 13, 11,  6,
-		 4,  3,  2, 12, 9,  5, 15, 10, 11, 14,  1,  7,  6,  0,  8, 13,
+		12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11,
+		10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8,
+		9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6,
+		4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13,
 	},
 	{
-		 4, 11,  2, 14, 15, 0,  8, 13,  3, 12, 9,  7,  5, 10, 6,  1,
-		13,  0, 11,  7,  4, 9,  1, 10, 14,  3, 5, 12,  2, 15, 8,  6,
-		 1,  4, 11, 13, 12, 3,  7, 14, 10, 15, 6,  8,  0,  5, 9,  2,
-		 6, 11, 13,  8,  1, 4, 10,  7,  9,  5, 0, 15, 14,  2, 3, 12,
+		4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1,
+		13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6,
+		1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2,
+		6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12,
 	},
 	{
-		13,  2,  8, 4,  6, 15, 11,  1, 10,  9,  3, 14,  5,  0, 12,  7,
-		 1, 15, 13, 8, 10,  3,  7,  4, 12,  5,  6, 11,  0, 14,  9,  2,
-		 7, 11,  4, 1,  9, 12, 14,  2,  0,  6, 10, 13, 15,  3,  5,  8,
-		 2,  1, 14, 7,  4, 10,  8, 13, 15, 12,  9,  0,  3,  5,  6, 11,
+		13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7,
+		1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2,
+		7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8,
+		2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11,
 	},
 }
 
 var pBoxPermutation []int = []int{
-	16, 7, 20, 21, 29, 12, 28, 17,  1, 15, 23, 26,  5, 18, 31, 10,
-	 2, 8, 24, 14, 32, 27,  3,  9, 19, 13, 30,  6, 22, 11,  4, 25,
+	16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5, 18, 31, 10,
+	2, 8, 24, 14, 32, 27, 3, 9, 19, 13, 30, 6, 22, 11, 4, 25,
 }
 
 var inputFile string
 var outputFile string
-var key string
+var keyString string
 var operation int
 
 func processArgs() {
@@ -118,7 +121,12 @@ func processArgs() {
 	outputParam := flag.String("o", "", "Output file")
 	keyParam := flag.String("k", "", "Cipher key")
 	flag.Parse()
-	if *keyParam == "" {
+	if !*encrypt && !*decrypt || *encrypt {
+		operation = ENCRYPT
+	} else {
+		operation = DECRYPT
+	}
+	if *keyParam == "" && operation == DECRYPT {
 		fmt.Println("Cipher key is required")
 		flag.PrintDefaults()
 		os.Exit(1)
@@ -127,21 +135,39 @@ func processArgs() {
 		fmt.Println("Choose between only one of -encrypt or -decrypt operations")
 		os.Exit(1)
 	}
-	if !*encrypt && !*decrypt || *encrypt {
-		operation = ENCRYPT
-	} else {
-		operation = DECRYPT
-	}
-	key = *keyParam
+	keyString = *keyParam
 	inputFile = *inputParam
 	outputFile = *outputParam
 	fmt.Println(inputFile)
 	fmt.Println(outputFile)
-	fmt.Println(key)
+}
+
+func prepareKey() (result []byte) {
+	rand.Seed(time.Now().UnixNano())
+	if keyString == "" {
+		result = make([]byte, 8)
+		for i := range result {
+			result[i] = byte(rand.Intn(256))
+		}
+	} else {
+		keyString = fmt.Sprintf("%016s", keyString)
+		keyString = keyString[0:16]
+		var err error
+		result, err = hex.DecodeString(keyString)
+		if err != nil {
+			fmt.Printf("Key %s is not a valid hexadecimal value")
+			os.Exit(1)
+		}
+	}
+	return result
+}
+
+func blockToHexString(block []byte) string {
+	return hex.EncodeToString(block)
 }
 
 func permutate(block []byte, permutationMatrix []int) (result []byte) {
-	result = make([]byte, len(permutationMatrix) >> 3)
+	result = make([]byte, len(permutationMatrix)>>3)
 	for i := 0; i < len(permutationMatrix); i++ {
 		resByteIndex := i >> 3
 		resBitIndex := i - (resByteIndex << 3)
@@ -151,25 +177,25 @@ func permutate(block []byte, permutationMatrix []int) (result []byte) {
 		blockByte := block[blockByteIndex]
 		resByte := result[resByteIndex]
 
-		var bit byte = (0x01 & (blockByte >> uint(7 - blockBitIndex))) << uint(7 - resBitIndex)
+		var bit byte = (0x01 & (blockByte >> uint(7-blockBitIndex))) << uint(7-resBitIndex)
 
-		result[resByteIndex] = (resByte & ^(0x01 << uint(7 - resBitIndex))) | bit
+		result[resByteIndex] = (resByte & ^(0x01 << uint(7-resBitIndex))) | bit
 	}
 	/*
-	for i := 0; i < len(permutationMatrix); i++ {
-		byte1Index := i >> 3
-		bit1Index := i - (byte1Index << 3)
-		byte2Index := (permutationMatrix[i] - 1) >> 3
-		bit2Index := (permutationMatrix[i] - 1) - (byte2Index << 3)
-		byte1 := block[byte1Index]
-		byte2 := block[byte2Index]
+		for i := 0; i < len(permutationMatrix); i++ {
+			byte1Index := i >> 3
+			bit1Index := i - (byte1Index << 3)
+			byte2Index := (permutationMatrix[i] - 1) >> 3
+			bit2Index := (permutationMatrix[i] - 1) - (byte2Index << 3)
+			byte1 := block[byte1Index]
+			byte2 := block[byte2Index]
 
-		var bit1 byte = (0x01 & (byte1 >> uint(7 - bit1Index))) << uint(7 - bit2Index)
-		var bit2 byte = (0x01 & (byte2 >> uint(7 - bit2Index))) << uint(7 - bit1Index)
+			var bit1 byte = (0x01 & (byte1 >> uint(7 - bit1Index))) << uint(7 - bit2Index)
+			var bit2 byte = (0x01 & (byte2 >> uint(7 - bit2Index))) << uint(7 - bit1Index)
 
-		block[byte1Index] = (byte1 & ^(0x01 << uint(7 - bit1Index))) | bit2
-		block[byte2Index] = (byte2 & ^(0x01 << uint(7 - bit2Index))) | bit1
-	}
+			block[byte1Index] = (byte1 & ^(0x01 << uint(7 - bit1Index))) | bit2
+			block[byte2Index] = (byte2 & ^(0x01 << uint(7 - bit2Index))) | bit1
+		}
 	*/
 	return result
 }
@@ -187,7 +213,7 @@ func leftRoundShift(block []byte, shift int, beginOffset int, endOffset int) (re
 	}
 	for i := 0; i < shift; i++ {
 		var leading byte = 0x00
-		for j := len(result)-1; j >= 0; j-- {
+		for j := len(result) - 1; j >= 0; j-- {
 			var nextLeading byte = (result[j] & 0x80) >> 7
 			result[j] = (result[j] << 1) | leading
 			leading = nextLeading
@@ -208,7 +234,7 @@ func joinKeys(left []byte, right []byte) (result []byte) {
 	}
 	result[3] = (left[3] & 0xf0) | (0x0f & right[0])
 	for i := 1; i < 4; i++ {
-		result[3 + i] = right[i]
+		result[3+i] = right[i]
 	}
 	return result
 }
@@ -225,11 +251,8 @@ func getRoundSubkey(key []byte, round int) (result []byte) {
 	return result
 }
 
-func generateRoundKeys() (result [][]byte) {
-	block := make([]byte, 8)
-	transformedKey := key64To56(block)
-	transformedKey[0] = 0x80
-	transformedKey[len(transformedKey)-1] = 0x01;
+func generateRoundKeys(key []byte) (result [][]byte) {
+	transformedKey := key64To56(key)
 	result = make([][]byte, 16)
 	for i := 0; i < 16; i++ {
 		result[i] = getRoundSubkey(transformedKey, i)
@@ -237,11 +260,39 @@ func generateRoundKeys() (result [][]byte) {
 	return result
 }
 
+func getNextDataBlock() (result []byte) {
+	result, _ = hex.DecodeString("ff00ff00ff00ff00")
+	return result
+}
+
+func xorBlocks(block1 []byte, block2 []byte) (result []byte) {
+	result = make([]byte, len(block1))
+	for i := range(block1) {
+		result[i] = block1[i] ^ block2[i]
+	}
+	return result
+}
+
+func encryptBlock(key []byte, block []byte) (result []byte) {
+	left := block[0:4]
+	right := block[3:]
+	expandedRight := permutate(right, expansionPermutation)
+	right = xorBlocks(expandedRight, key)
+	result = append(left, right...)
+	// s-box
+	// p-box
+	return result
+}
+
 func main() {
-	// @TODO Comando para gerar chave
 	// @TODO Suportar standard input
 	// @TODO Suportar standard output
-	roundKeys := generateRoundKeys()
-	fmt.Println(roundKeys)
 	processArgs()
+	key := prepareKey()
+	fmt.Printf("Using key: %s\n", blockToHexString(key))
+	roundKeys := generateRoundKeys(key)
+	data := getNextDataBlock()
+	for k := range(roundKeys) {
+		data = encryptBlock(roundKeys[k], data)
+	}
 }
